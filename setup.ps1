ba9cbf4ad193630383a1ec45c3f30396f5f02443
siun <siuncyclone@gmail.com>
2015-04-12 14:12:28 +0900

    Change Invoke-WebRequest to DownloadFile

--- setup.ps1
+++ setup.ps1
@@ -5,7 +5,7 @@ if ((Test-Path 'C:\Program Files (x86)\Mozilla Firefox') -Or (Test-Path 'C:\Prog
   echo "firefox is already installed"
 } else {
   echo "*** Downloading firefox ***"
-  Invoke-WebRequest "https://download.mozilla.org/?product=firefox-36.0.4-SSL&os=win&lang=ja" -OutFile $HOME/firefox_installer.exe -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Firefox
+  (New-Object System.Net.WebClient).DownloadFile("https://download.mozilla.org/?product=firefox-36.0.4-SSL&os=win&lang=ja", "$HOME/firefox_installer.exe")
   Start-Process $HOME/firefox_installer.exe
 }
 
@@ -15,9 +15,9 @@ if ((Test-Path 'C:\msys64') -Or (Test-Path 'C:\msys32')) {
 } else {
   echo "*** Downloading msys2 ***"
   if ([System.Environment]::Is64BitOperatingSystem) {
-    Invoke-WebRequest "http://downloads.sourceforge.net/project/msys2/Base/x86_64/msys2-x86_64-20150202.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmsys2%2Ffiles%2FBase%2Fx86_64%2Fmsys2-x86_64-20150202.exe%2Fdownload&ts=1427130794&use_mirror=jaist" -OutFile $HOME/msys2_installer.exe -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Firefox
+    (New-Object System.Net.WebClient).DownloadFile("http://downloads.sourceforge.net/project/msys2/Base/x86_64/msys2-x86_64-20150202.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmsys2%2Ffiles%2FBase%2Fx86_64%2Fmsys2-x86_64-20150202.exe%2Fdownload&ts=1427130794&use_mirror=jaist", "$HOME/msys2_installer.exe")
   } else {
-    Invoke-WebRequest "http://downloads.sourceforge.net/project/msys2/Base/i686/msys2-i686-20150202.exe?r=&ts=1427333443&use_mirror=jaist" -OutFile $HOME/msys2_installer.exe -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Firefox
+    (New-Object System.Net.WebClient).DownloadFile("http://downloads.sourceforge.net/project/msys2/Base/i686/msys2-i686-20150202.exe?r=&ts=1427333443&use_mirror=jaist", "$HOME/msys2_installer.exe")
   }
   Start-Process $HOME/msys2_installer.exe -Wait
 }
