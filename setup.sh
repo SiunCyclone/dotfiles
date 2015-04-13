bab6ea23bbe75e56192d12a011a41e8509970db2
siun <siuncyclone@gmail.com>
2015-04-13 21:07:42 +0900

    Set zsh as a login shell

--- setup.sh
+++ setup.sh
@@ -85,12 +85,16 @@ function install_brew_pkg {
 }
 
 function modify_msys_batfile {
+  echo "*** Modify msys2 batfile ***"
+  # Enable symlink
+  # Set zsh as login shell
   ruby -e "
     text = ''
     msysPath = /i(3|6)86/ =~ \`uname -a\`.to_s ? 'C:/msys32/' : 'C:/msys64/'
     open(msysPath + 'msys2_shell.bat') do |f|
       text = f.read
       text.gsub!('rem set MSYS=winsymlinks:nativestrict', 'set MSYS=winsymlinks:nativestrict')
+      text.gsub!('start %WD%mintty -i /msys2.ico /usr/bin/bash --login %*', 'start %WD%mintty -i /msys2.ico /usr/bin/zsh --login %*')
     end
 
     open(msysPath + 'msys2_shell.bat', 'w') do |f|
